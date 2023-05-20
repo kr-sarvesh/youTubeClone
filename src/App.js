4234669515d7700a72fcc06b8f59eb1b4d983721
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-05-21 00:34:49 +0530

    Provider added to app component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import Header from './components/Header'
 import Body from './components/Body'
-
+import { Provider } from 'react-redux'
+import store from './utils/store'
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <div className='container mx-auto'>
         <Header />
         <Body />
       </div>
-    </>
+    </Provider>
   )
 }
 
