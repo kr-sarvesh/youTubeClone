4dad785c7eee06f27320b3be67bc70b17b5d10d4
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-05-22 19:13:43 +0530

    added button on body

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+const Button = ({ name }) => {
+  return (
+    <div>
+      <button className='px-5 py-2 m-2 bg-gray-200 rounded-lg hover:bg-gray-300'>
+        {name}
+      </button>
+    </div>
+  )
+}
+export default Button
--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,5 +1,15 @@
+import Button from './Button'
+
+//List of  Buttons
+const list = ['All', 'Gaming', 'Work', 'Sports', 'cricket', 'Chores', 'Other']
 const ButtonList = () => {
-  return <div>ButtonList</div>
+  return (
+    <div className='flex'>
+      {list.map((item) => (
+        <Button key={item} name={item} />
+      ))}
+    </div>
+  )
 }
 
 export default ButtonList
