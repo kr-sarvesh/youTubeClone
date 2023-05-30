e56b5efe9d93d7663d8350367dd128c1cb212b56
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-05-30 19:42:46 +0530

    setting up nested comment feat

--- src/components/commentsContainer.js
+++ src/components/commentsContainer.js
@@ -1,13 +1,66 @@
+const commentData = [
+  {
+    name: 'John Doe',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+    replies: [],
+  },
+  {
+    name: 'John Doe',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+    replies: [
+      {
+        name: 'John Doe',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+        replies: [
+          {
+            name: 'John Doe',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+            replies: [],
+          },
+        ],
+      },
+      {
+        name: 'John Doe',
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+        replies: [
+          {
+            name: 'John Doe',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+            replies: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: 'John Doe',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+    replies: [],
+  },
+  {
+    name: 'John Doe',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
+    replies: [],
+  },
+]
+
+const Comment = ({ data }) => {
+  const { name, text, replies } = data
+  return (
+    <div>
+      <img
+        src='https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png'
+        alt='no-profile'
+      />
+    </div>
+  )
+}
+
 function CommentsContainer() {
-  const commentData = [
-    {
-      name: 'John Doe',
-      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-    },
-  ]
   return (
     <div>
       <h1 className='text-xl py-4 px-5 font-bold'>Comments:</h1>
+      <Comment data={commentData[0]} />
     </div>
   )
 }
