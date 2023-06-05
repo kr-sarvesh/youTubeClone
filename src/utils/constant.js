20bbe229f1a53eb31baed5cfbd3d6ffb932477ef
Kumar Sarvesh <kr.sr.rssb@gmail.com>
2023-06-05 17:22:38 +0530

    added htttps

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,4 +3,4 @@ export const YOUTUBE_VIDEOS_API =
   process.env.REACT_APP_YOUTUBE_API_KEY
 
 export const YOUTUBE_SEARCH_API =
-  'http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q='
+  'https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q='
